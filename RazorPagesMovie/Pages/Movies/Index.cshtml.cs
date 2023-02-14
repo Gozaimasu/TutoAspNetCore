40bf2b93c2e26ccac004fd318324676d24ad21b3
Olivier DAHON <odahon@index-education.fr>
2023-02-14 12:47:55 +0100

    Recherche par titre

--- RazorPagesMovie/Pages/Movies/Index.cshtml.cs
+++ RazorPagesMovie/Pages/Movies/Index.cshtml.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPagesMovie.Data;
 using RazorPagesMovie.Models;
@@ -16,8 +18,23 @@ public class IndexModel : PageModel
 
     public IList<Movie> Movie { get;set; } = default!;
 
+    [BindProperty(SupportsGet = true)]
+    public string? SearchString { get; set; }
+
+    public SelectList? Genres { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public string? MovieGenre { get; set; }
+
     public async Task OnGetAsync()
     {
-        Movie = await _context.Movie.ToListAsync();
+        var movies = from m in _context.Movie
+                     select m;
+        if (!string.IsNullOrEmpty(SearchString))
+        {
+            movies = movies.Where(s => s.Title != null && s.Title.Contains(SearchString));
+        }
+
+        Movie = await movies.ToListAsync();
     }
 }
