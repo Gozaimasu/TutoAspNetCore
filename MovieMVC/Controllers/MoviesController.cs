479b0afd1a0879e943acd8c3b58ab44a49e0f28e
Olivier DAHON <odahon@index-education.fr>
2023-02-28 12:39:01 +0100

    Création d'un film

--- MovieMVC/Controllers/MoviesController.cs
+++ MovieMVC/Controllers/MoviesController.cs
@@ -1,25 +1,54 @@
-﻿using CleanMovie.Application.UseCases.GetMovies;
+﻿using CleanMovie.Application.UseCases.CreateMovie;
+using CleanMovie.Application.UseCases.GetMovies;
 using Microsoft.AspNetCore.Mvc;
 using MovieMVC.Models;
 
 namespace MovieMVC.Controllers;
 
-public class MoviesController : Controller, IGetMoviesOutputPort
+public class MoviesController : Controller, IGetMoviesOutputPort, IOutputPort
 {
     private readonly IGetMoviesUseCase _getMoviesUseCase;
+    private readonly ICreateMovieUseCase _createMovieUseCase;
 
-    public MoviesController(IGetMoviesUseCase getMoviesUseCase)
+    private ActionResult? result;
+
+    public MoviesController(
+        IGetMoviesUseCase getMoviesUseCase,
+        ICreateMovieUseCase createMovieUseCase)
     {
         _getMoviesUseCase = getMoviesUseCase;
+        _createMovieUseCase = createMovieUseCase;
     }
 
-    public async Task<IActionResult> Index(string? searchString, string? movieGenre)
+    public async Task<ActionResult> Index(string? searchString, string? movieGenre)
     {
         _getMoviesUseCase.SetOutputPort(this);
 
         await _getMoviesUseCase.ExecuteAsync(searchString, movieGenre);
 
+        return result!;
+    }
+
+    public ActionResult Create()
+    {
         return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> Create([Bind("ID,Title,ReleaseDate,Genre,Price, Rating")] Movie movie)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(movie);
+        }
+
+        _createMovieUseCase.SetOutputPort(this);
+
+        await _createMovieUseCase.ExecuteAsync(movie.Title!, movie.ReleaseDate, movie.Genre!, movie.Price, movie.Rating);
+        
+        return result!;
+
     }
 
     #region IGetMoviesOutputPort
@@ -37,7 +66,24 @@ public class MoviesController : Controller, IGetMoviesOutputPort
                 Title = m.Title
             }).ToList()
         };
-        ViewData.Model = model;
+        result = View(model);
+    }
+    #endregion
+
+    #region IOutputPort
+    void IOutputPort.Ok(CleanMovie.Domain.Movie movie)
+    {
+        result = RedirectToAction("Index");
+    }
+
+    void IOutputPort.NotFound()
+    {
+        result = NotFound();
+    }
+
+    void IOutputPort.Invalid()
+    {
+        result = View();
     }
     #endregion
 }
