d2d749b7debda0ad87d496d85dc053a2283b5158
Olivier DAHON <odahon@index-education.fr>
2023-02-28 12:25:59 +0100

    Affichage de la liste des films

---
+++ MovieMVC/Controllers/MoviesController.cs
@@ -0,0 +1,43 @@
+﻿using CleanMovie.Application.UseCases.GetMovies;
+using Microsoft.AspNetCore.Mvc;
+using MovieMVC.Models;
+
+namespace MovieMVC.Controllers;
+
+public class MoviesController : Controller, IGetMoviesOutputPort
+{
+    private readonly IGetMoviesUseCase _getMoviesUseCase;
+
+    public MoviesController(IGetMoviesUseCase getMoviesUseCase)
+    {
+        _getMoviesUseCase = getMoviesUseCase;
+    }
+
+    public async Task<IActionResult> Index(string? searchString, string? movieGenre)
+    {
+        _getMoviesUseCase.SetOutputPort(this);
+
+        await _getMoviesUseCase.ExecuteAsync(searchString, movieGenre);
+
+        return View();
+    }
+
+    #region IGetMoviesOutputPort
+    void IGetMoviesOutputPort.Ok(IEnumerable<CleanMovie.Domain.Movie> movie)
+    {
+        var model = new MovieGenreViewModel()
+        {
+            Movies = movie.Select(m => new Movie
+            {
+                Genre = m.Genre,
+                Id = m.Id,
+                Price = m.Price,
+                Rating = m.Rating,
+                ReleaseDate = m.ReleaseDate,
+                Title = m.Title
+            }).ToList()
+        };
+        ViewData.Model = model;
+    }
+    #endregion
+}
---
+++ MovieMVC/Models/Movie.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieMVC.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+
+    public string? Title { get; set; }
+
+    [Display(Name = "Release Date")]
+    public DateTime ReleaseDate { get; set; }
+
+    public string? Genre { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string Rating { get; set; } = string.Empty;
+}
